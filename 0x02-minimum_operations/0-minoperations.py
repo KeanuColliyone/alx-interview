cfc4480d5629d368977ad053f701b377f5e3d0b5
Kenan Kotzee <frostcolliyone@gmail.com>
2024-10-18 02:25:12 +0000

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,7 +7,7 @@ It calculates the fewest number of operations needed to achieve n H characters.
 
 def minOperations(n):
     """
-    Calculates the minimum number of operations required to result in exactly n H characters
+    Calculates the minimum number of operations required.
     using only Copy All and Paste operations.
 
     Args:
@@ -19,14 +19,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
