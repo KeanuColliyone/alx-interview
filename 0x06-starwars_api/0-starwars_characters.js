fe073483aa90e595cbaffe0d91f699b2f35750fa
Kenan Kotzee <frostcolliyone@gmail.com>
2024-11-14 11:20:09 -0800

    0x06-starwars_api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body).name;
+      console.log(character);
+    });
+  });
+});
